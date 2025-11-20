d6e5fa04c5a904b0d0d9300e84b5c5bf1e62359e
Dani Weatherwax <dweatherwax@ucsd.edu>
2025-11-19 21:36:34 -0800

    Class 5-10 files

---
+++ Class07.R
@@ -0,0 +1,10 @@
+> Video recap: Machine learning can be sunspuervised, unsupervised, or reinforced
+> Clustering
+- K- means clustering
+- Hierarchichal clustering
+
+- Dimensionality reduction, vixualization, and 'structure' analysis
+-Principal component analysis (PCA)
+
+Kmeans() 
+#generate some example data for clustering
